23f0135dc4f70253ddb345fbdac826191af94732
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2024-10-14 00:03:11 +0530

    did changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {  clearProfilestuffs, editProfile, editobj, findUser, follow, getUnique, getUserDetails, getfollowers, getfollowing, getotherUser, getuserskill, like, loginOtp, logout, otpSendToVerify, setProfile, usernameAvia, verifyLoginOtp, verifyOtp } from "../controllers/userControllers.js";
+import {  clearProfilestuffs, editProfile, editobj, findUser, follow, getUnique, getUserDetails  , getfollowing, getotherUser, getuserskill, like, loginOtp, logout, otpSendToVerify, setProfile, usernameAvia, verifyLoginOtp, verifyOtp } from "../controllers/userControllers.js";
 import { isAuthenticatedUser } from "../middleware/auth.js";
 import { upload } from "../middleware/multer.js";
 import { getNotification } from "../controllers/notificationcontrollers.js";
@@ -25,7 +25,7 @@ router.route('/like/:username').put(isAuthenticatedUser, like);
 router.route('/connect/:username').put(isAuthenticatedUser, follow);
 router.route('/:username').get(getotherUser);
 router.route("/notification/:notificationId").get(isAuthenticatedUser, getNotification)
-router.route("/getfollowers/:userid").get(getfollowers);
+//router.route("/getfollowers/:userid").get(getfollowers);
 router.route("/getfollowing/:userid").get(getfollowing);
 router.route("/getuserbyskill").post(getuserskill);
 router.route("/useravia").post(isAuthenticatedUser, usernameAvia);
