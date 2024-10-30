3e4ac0d17ec1a1233d0759ca5304eb06a3da737e
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2024-10-30 09:45:18 +0530

    chb

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -35,7 +35,7 @@ const generateAndsaveTokens = async(user, res)=>{
         res.cookie('accessToken',
             accessToken,{
                 httpOnly: true,
-                secure: false,
+                secure: true,
                 sameSite: 'None',
                 expires: new Date( Date.now() + 15*24*60*60*1000 ),
                 path:'/'
